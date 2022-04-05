a36845be9e569504fc3206e0bac9911466ecc25d
Amrin <amrin.sandhar@gmail.com>
2022-04-04 18:34:23 -0700

    initial commit

---
+++ src/main/java/com/rmrfroot/tasktracker222/TaskTracker222Application.java
@@ -0,0 +1,13 @@
+package com.rmrfroot.tasktracker222;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskTracker222Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskTracker222Application.class, args);
+    }
+
+}
