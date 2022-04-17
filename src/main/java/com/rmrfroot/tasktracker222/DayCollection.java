d68482e02291a78115e744d55c94b282251520cb
Noel Ian Paulite <npaulite@csus.edu>
2022-04-16 19:33:49 -0700

    Day Collection start

---
+++ src/main/java/com/rmrfroot/tasktracker222/DayCollection.java
@@ -0,0 +1,21 @@
+package com.rmrfroot.tasktracker222;
+
+public class DayCollection {
+    // I dont know what the Day object is
+
+    //array ? lists? queues? or hash?
+    private int size;
+
+    //public void add(Day day) {
+
+    //public void remove()
+
+    //public void isEmpty()
+
+    //public DayCollection getCollection()
+
+    public int getSize() {
+        return size;
+    }
+
+}
