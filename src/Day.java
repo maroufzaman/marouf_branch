022f1fe3f5bcd45011ed1c046036da95ec07221c
Syed Zaman <faroukzaman29@gmail.com>
2022-04-24 19:45:25 -0700

    #Author: Marouf Zaman #Jira flying number 116 #create Day object #Store the assigned task to be completed and training to do for the day

--- src/Day.java
+++ src/Day.java
@@ -1,16 +1,22 @@
 import java.time.DayOfWeek;
 import java.util.*;
 
-public class Day {
+public class Day implement Comparable{
 
     // declare variables
     private DayOfWeek dayOfWeek;
+
+    private String name;
+
+    private int age
 
     private int month;
 
     private int year;
 
     private String task;
+
+    public String training;
 
     private boolean done = false;
 
@@ -32,16 +38,25 @@ public class Day {
     }
 
 
-    //set the assigned task for the day
+    //set the assigned task to be completed for the day
     public void setTask() {
 
 
-        System.out.println("Enter the task : ");
+        System.out.println("Enter the task to be completed : ");
 
         Scanner input = new Scanner(System.in);
 
         task = input.nextLine();
 
+    }
+
+    //set the training to do for the day
+    public void setTraining(){
+        System.out.println("Enter the training to do: ");
+
+        Scanner input = new Scanner(System.in);
+
+        training = input.nextLine();
     }
 
     // set the  day for the assigned task
@@ -127,6 +142,51 @@ public class Day {
 
     }
 
+    //set the name of the officer who is assigned to do the task
+    public void setName(){
+        System.out.println("Enter the officer name: ");
+        Scanner input = new Scanner(System.in);
+
+        name = input.nextLine();
+    }
+
+    //set the officer age
+    public void setAge(){
+
+        System.out.println("Enter the officer age: ");
+        Scanner input = new Scanner(System.in);
+
+        age = input.nextInt();
+    }
+
+    //compare a Day object with another Day object
+    public int compareTo(Day other){
+
+        if (year < other.getYear())
+            return -1
+        else if (year == other.getYear()){
+
+            if (month < other.getMonth())
+                return -1
+             else if (month  == other.getMonth()){
+
+                if (dayOfWeek < other.getDayOfWeek())
+                    return -1'
+                else if (dayOfWeek  == other.getDayOfWeek())
+                    return 0;
+                else if ( dayOfWeek > other.getDayOfWeek())
+                    return 1;
+
+            }
+
+            else if ( month > other.getMonth())
+                return 1;
+        }
+        else if ( year > other.getYear())
+            return 1;
+
+    }
+
     //set the year for assigned task
     public void setYear() {
 
@@ -139,9 +199,44 @@ public class Day {
 
     }
 
+    // set the  day for the assigned task
+    public void setDayOfWeek(DayOfWeek dayOfWeek) {
+        this.dayOfWeek = dayOfWeek;
+    }
+
+    //set the month for assigned task
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    //set the year for assigned task
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    //set the assigned task for the day
+    public void setTask(String task) {
+        this.task = task;
+    }
+
+    //set the training for the day
+    public void setTraining(String training) {
+        this.training = training;
+    }
+
     //set the done to true or false  for assigned task
     public void setDone(boolean x){
         done = x;
+    }
+
+    //set the name of the officer who is assigned to do the task
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    //set the officer age
+    public void setAge(int age) {
+        this.age = age;
     }
 
     //get the year
@@ -159,9 +254,28 @@ public class Day {
         return task;
     }
 
+   //get the training for the day
+    public String getTraining() {
+        return training;
+    }
+
     //cheek whether the assigned task is done
     public boolean isDone(){
         return done;
     }
 
+    //get the day of week for the assigned task
+    public DayOfWeek getDayOfWeek() {
+        return dayOfWeek;
+    }
+
+    //get the officer name assigned for the task
+    public String getName() {
+        return name;
+    }
+
+    //get the officer age
+    public int getAge() {
+        return age;
+    }
 }
