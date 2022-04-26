ee19692e3de3c2af5208ef8a58eedca90b014679
Brian Frey <bfrey@csus.edu>
2022-04-25 18:40:42 -0700

    Drill object class.

---
+++ src/main/java/com/rmrfroot/tasktracker222/Drills/Drill.java
@@ -0,0 +1,34 @@
+package com.rmrfroot.tasktracker222.Drills;
+
+import com.rmrfroot.tasktracker222.Day;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="drills")
+public class Drill {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+
+    @Column(name="drill_name")
+    private String drillName;
+
+    @Column(name="drill_day")
+    private Day drillDay;
+
+    public void setId(int drill_id){this.id = drill_id;}
+
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDrillName() {
+        return drillName;
+    }
+
+    public Day getDay(){return drillDay;}
+}
