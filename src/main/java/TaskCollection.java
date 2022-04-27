be878d05013d278fb1d00536dbc01d5049801663
Syed Zaman <faroukzaman29@gmail.com>
2022-04-27 08:17:38 -0700

    #Author: Marouf Zaman #Jira flying number 126 and 127 #create Task object and TaskCollection object #make changes to the TaskCollection object

--- src/main/java/TaskCollection.java
+++ src/main/java/TaskCollection.java
@@ -3,65 +3,67 @@ import java.util.*;
 public class TaskCollection {
 
     //declare variables
-    private List<Task> task  =  new ArrayList<Task>();
+    private List<Task> taskCollection = new ArrayList<Task>();
     private int priority;
     private int index;
 
     //constructor
-    public TaskCollection(){
-
+    public TaskCollection() {
+            index = 0;
     }
 
     //add a Task object to the collection
-public void addTask(Task t){
-        task.add(t);
+    public void addTask(Task task) {
+        taskCollection.add(task);
+          index++;
+    }
 
-}
+    //remove the Task object
+    public void removeTask(Task task) {
+      int  i = taskCollection.indexOf(task);
+        taskCollection.remove(i);
+    }
 
-//remove the Task object
-public void removeTask(Task t){
-      index =  task.indexOf(t);
-        task.remove(index);
-}
-
-//set the priority of the task object
-public void setPriority(){
+    //set the priority of the task object
+    public void setPriority() {
 
         Scanner input = new Scanner(System.in);
         System.out.println("Number 1 will represent higher priority and Number 2 will represent lower priority");
         System.out.println("Enter 1 or 2 to determine the priority of the task");
-        priority =  input.nextInt();
-}
+        priority = input.nextInt();
+    }
 
-//get Priority
+    //get Priority
     public int getPriority() {
         return priority;
     }
 
     //get the number of Task object
-    public int size(){
-     return  task.size();
-}
+    public int size() {
+        return taskCollection.size();
+    }
 
-//clear all Task object
-public void clearAll(){
-        task.clear();
-}
+    //clear all Task object
+    public void clearAll() {
+        taskCollection.clear();
+    }
 
-//check whether their is a  task object
-public boolean containsTask(Task t){
-       return task.contains(t);
-}
+    //check whether there is a  task object
+    public boolean containsTask(Task task) {
+        return taskCollection.contains(task);
+    }
 
-//get the Task object
-public Task getTask(){
-     return  task.get(index);
-}
+    //get the Task object
+    public Task getTask() {
+        return taskCollection.get(index);
+    }
 
-//Check whether the TaskCollection is empty
-public boolean isEmpty(){
-        return  task.isEmpty();
-}
+    //Check whether the TaskCollection is empty
+    public boolean isEmpty() {
+        return taskCollection.isEmpty();
+    }
+
+
 
 
 }
