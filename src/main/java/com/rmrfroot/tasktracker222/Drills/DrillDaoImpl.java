e399654af945f0c2df7f59f45df42b0c3fe97669
Amrin <amrin.sandhar@gmail.com>
2022-09-18 18:48:53 -0700

    deleted duplicate drill class

--- src/main/java/com/rmrfroot/tasktracker222/Drills/DrillDaoImpl.java
+++ src/main/java/com/rmrfroot/tasktracker222/Drills/DrillDaoImpl.java
@@ -1,14 +1,15 @@
-package com.rmrfroot.tasktracker222.Drills;
+package com.rmrfroot.tasktracker222.drills;
 
 
 import com.rmrfroot.tasktracker222.entities.Day;
+import com.rmrfroot.tasktracker222.entities.Drill;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
 import java.util.List;
 
-public class DrillDaoImpl implements DrillDao {
+public class DrillDaoImpl implements com.rmrfroot.tasktracker222.Drills.DrillDao {
 
     private Drill drill;
     private List<Drill> drillList;
