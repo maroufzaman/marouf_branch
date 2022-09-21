420c6f4146adc73038715b2efa431e18e1ecf55c
tmaduchukwu <tmaduchukwu22@gmail.com>
2022-09-20 21:16:31 -0700

    Removed unused import

--- src/main/java/com/rmrfroot/tasktracker222/services/DaoServiceImpl.java
+++ src/main/java/com/rmrfroot/tasktracker222/services/DaoServiceImpl.java
@@ -2,7 +2,6 @@ package com.rmrfroot.tasktracker222.services;
 
 import com.rmrfroot.tasktracker222.DAO.DaysDAO;
 import com.rmrfroot.tasktracker222.entities.Day;
-import com.rmrfroot.tasktracker222.entities.Drill;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
