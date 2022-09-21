f2031e281a704deaa449cd5e928ea82ef084b53b
Amrin <amrin.sandhar@gmail.com>
2022-09-20 18:09:02 -0700

    DT-27, DT-30 : Set up controllers for drill scheduler for both recipient and manager. Added ancillary security configurations that assist in fetching session details (specifically, username)

---
+++ src/main/java/com/rmrfroot/tasktracker222/configurers/MethodSecurityConfigurer.java
@@ -0,0 +1,11 @@
+package com.rmrfroot.tasktracker222.configurers;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
+
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class MethodSecurityConfigurer
+        extends GlobalMethodSecurityConfiguration {
+}
